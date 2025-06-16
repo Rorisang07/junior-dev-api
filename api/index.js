8eeb8accbb05384d4015016df12e4bbdcfc7626e
Rorisang Kgwete <rorisang050@gmail.com>
2025-06-16 12:21:13 +0200

    Remove local testing code for deployment

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/sort-string', (req, res) => {
   const { input } = req.body;
   if (!input || typeof input !== 'string') {
     return res.status(400).json({ error: 'Please provide a valid string in "input"' });
   }
-  const sorted = input.split('').sort();
-  res.json({ word: sorted });
-
+  const characters = input.split('').sort();
+  res.json({ word: characters });
 });
+
 module.exports = app;
